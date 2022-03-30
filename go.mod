a217c796d0806c6ceeb85230eee19a3c53bd0288
julien <julien@odatam.com>
2022-03-30 16:17:38 +0200

    Use base58 module

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/keys-pub/go-libfido2 v1.5.2
+	github.com/odatam/base58 v0.0.0-20220330141145-f99ee39764ff // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
 )
--- gofido2pass.go
+++ gofido2pass.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "github.com/keys-pub/go-libfido2"
+    "github.com/odatam/base58"    
 	"encoding/hex"
 	"log"
 	"encoding/json"
@@ -18,132 +19,7 @@ import (
 	"path/filepath"
 	"os/exec"
 	"errors"
-	
-	"math/big"
 )
-
-// base58 encode from https://github.com/m0t0k1ch1/base58/blob/master/base58.go
-const (
-	Base58Chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
-	Base               = 58
-)
-
-var (
-	ErrInvalidLengthBytes = errors.New("invalid length bytes")
-	ErrInvalidChar        = errors.New("invalid char")
-)
-
-type Base58 struct {
-	chars      [58]byte
-	charIdxMap map[byte]int64
-}
-
-func NewBase58(charsStr string) (*Base58, error) {
-	b58 := &Base58{}
-
-	if err := b58.initChars(charsStr); err != nil {
-		return nil, err
-	}
-
-	if err := b58.initCharIdxMap(charsStr); err != nil {
-		return nil, err
-	}
-
-	return b58, nil
-}
-
-func (b58 *Base58) initChars(charsStr string) error {
-	if len(charsStr) != 58 {
-		return ErrInvalidLengthBytes
-	}
-
-	chars := []byte(charsStr)
-	copy(b58.chars[:], chars[:])
-
-	return nil
-}
-
-func (b58 *Base58) initCharIdxMap(charsStr string) error {
-	if len(charsStr) != 58 {
-		return ErrInvalidLengthBytes
-	}
-
-	b58.charIdxMap = map[byte]int64{}
-	for i, b := range []byte(charsStr) {
-		b58.charIdxMap[b] = int64(i)
-	}
-
-	return nil
-}
-
-func (b58 *Base58) EncodeToString(b []byte) (string, error) {
-	n := &big.Int{}
-	n.SetBytes(b)
-
-	buf := &bytes.Buffer{}
-	for _, c := range b {
-		if c == 0x00 {
-			if err := buf.WriteByte(b58.chars[0]); err != nil {
-				return "", err
-			}
-		} else {
-			break
-		}
-	}
-
-	zero := big.NewInt(0)
-	div := big.NewInt(Base)
-	mod := &big.Int{}
-
-	tmpBuf := &bytes.Buffer{}
-	for {
-		if n.Cmp(zero) == 0 {
-			tmpBytes := tmpBuf.Bytes()
-			for i := len(tmpBytes) - 1; i >= 0; i-- {
-				buf.WriteByte(tmpBytes[i])
-			}
-			return buf.String(), nil
-		}
-
-		n.DivMod(n, div, mod)
-		if err := tmpBuf.WriteByte(b58.chars[mod.Int64()]); err != nil {
-			return "", err
-		}
-	}
-}
-
-func (b58 *Base58) DecodeString(s string) ([]byte, error) {
-	b := []byte(s)
-
-	startIdx := 0
-	buf := &bytes.Buffer{}
-	for i, c := range b {
-		if c == b58.chars[0] {
-			if err := buf.WriteByte(0x00); err != nil {
-				return nil, err
-			}
-		} else {
-			startIdx = i
-			break
-		}
-	}
-
-	n := big.NewInt(0)
-	div := big.NewInt(Base)
-
-	for _, c := range b[startIdx:] {
-		charIdx, ok := b58.charIdxMap[c]
-		if !ok {
-			return nil, ErrInvalidChar
-		}
-
-		n.Add(n.Mul(n, div), big.NewInt(charIdx))
-	}
-
-	buf.Write(n.Bytes())
-
-	return buf.Bytes(), nil
-}
 
 type GoFido2PassItem struct { 
 	UserId string
@@ -293,7 +169,7 @@ func main() {
 	var hexBool bool
 	var base58Bool bool	
 	var lengthParam int
-		
+						
 	homeDir, err := os.UserHomeDir()
     if err != nil {
         log.Fatalln( err )
@@ -487,8 +363,7 @@ func main() {
 			if (hexBool) {
 				hashOutput = hex.EncodeToString(sliceHash)
 			} else {
-				b58, _ := NewBase58(Base58Chars)
-				hashOutput, err = b58.EncodeToString(sliceHash)
+				hashOutput, err = base58.Base58EncodeToString(sliceHash)
 				if err != nil {
 					log.Fatal(err)
 				}			
